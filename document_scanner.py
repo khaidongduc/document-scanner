847d89a74c93cdd85ca035f09600da55f0dcebc7
khaidongduc <khaidongduc@gmail.com>
2021-04-18 13:37:03 +0700

    user img original size instead of resizing

--- document_scanner.py
+++ document_scanner.py
@@ -21,7 +21,7 @@ NUM_SHRINKING_PIXEL = 5
 
 class DocumentScanner:
     def __init__(self,
-                 img_size=(IMG_WIDTH, IMG_HEIGHT),
+                 target_img_size=(IMG_WIDTH, IMG_HEIGHT),
                  canny_sigma=CANNY_SIGMA,
                  dilate_kernel=DILATE_KERNEL,
                  erode_kernel=ERODE_KERNEL,
@@ -31,7 +31,7 @@ class DocumentScanner:
                  contour_method=CONTOUR_METHOD,
                  min_contour_area=utils.MIN_CONTOUR_AREA,
                  num_shrinking_pixel=NUM_SHRINKING_PIXEL):
-        self.__img_width, self.__img_height = img_size
+        self.__img_width, self.__img_height = target_img_size
         self.__canny_sigma = canny_sigma
         self.__dilate_kernel = dilate_kernel
         self.__erode_kernel = erode_kernel
@@ -42,23 +42,21 @@ class DocumentScanner:
         self.__min_contour_area = min_contour_area
         self.__num_shrinking_pixel = num_shrinking_pixel
 
-    def scan_document(self, img_source):
-        img = cv2.imread(img_source)
-        imgResized = cv2.resize(img, (self.__img_width, self.__img_height))
-        imgProcessed = self.__process_img(imgResized)
-        biggest_contour_approx = self.__find_biggest_contour(imgProcessed, img)
-        imgWarped = self.__warp_image(imgResized, biggest_contour_approx)
+    def scan_document(self, img):
+        imgProcessed = self.__process_img(img)
+        biggest_contour_approx = self.__find_biggest_contour(imgProcessed)
+        imgWarped = self.__warp_image(img, biggest_contour_approx)
         return imgWarped
 
     def __process_img(self, img):
         imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to gray scale
-        imgBlur = cv2.GaussianBlur(imgGray, self.__gaussian_blur_kernel_size, self.__gaussian_blur_border_type)
+        imgBlur = cv2.GaussianBlur(imgGray, self.__gaussian_blur_kernel_size, self.__gaussian_blur_border_type, 0)
         imgEdged = utils.auto_canny(imgBlur, self.__canny_sigma)
         imgDilated = cv2.dilate(imgEdged, kernel=self.__dilate_kernel, iterations=2)
         imgEroded = cv2.erode(imgDilated, kernel=self.__erode_kernel, iterations=1)
         return imgEroded
 
-    def __find_biggest_contour(self, img, img_org):
+    def __find_biggest_contour(self, img):
         try:
             contours, hierarchy = cv2.findContours(img, self.__contour_mode, self.__contour_method)
             biggestContourApprox, maxArea = utils.find_biggest_contour_approx(contours, self. __min_contour_area)
@@ -72,7 +70,7 @@ class DocumentScanner:
         warped_perspective = np.float32([[0, 0], [self.__img_width, 0],
                                          [0, self.__img_height], [self.__img_width, self.__img_height]])
         transform_matrix = cv2.getPerspectiveTransform(contour_perspective, warped_perspective)
-        imgWarped = cv2.warpPerspective(img, transform_matrix, (IMG_WIDTH, IMG_HEIGHT))
+        imgWarped = cv2.warpPerspective(img, transform_matrix, (self.__img_width, self.__img_height))
         # correction
         imgWarped = imgWarped[self.__num_shrinking_pixel:imgWarped.shape[0] - self.__num_shrinking_pixel,
                               self.__num_shrinking_pixel:imgWarped.shape[1] - self.__num_shrinking_pixel]
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from document_scanner import DocumentScanner
 import os
-from cv2 import imshow, waitKey, imwrite
 
-document_scanner = DocumentScanner(canny_sigma=0.78)
+import cv2
+
+document_scanner = DocumentScanner(canny_sigma=0.8)
 
 sample_folder = "sample"
 result_folder = "result"
 
-file_name = "sample2.jpg"
+file_name = "sample1.jpg"
 sample_img_path = os.path.join(sample_folder, file_name)
-img = document_scanner.scan_document(sample_img_path)
+original_img = cv2.imread(sample_img_path)
+img = document_scanner.scan_document(original_img)
 
-imshow("Result", img)
-waitKey(0)
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
 
 result_img_path = os.path.join(result_folder, file_name)
-imwrite(result_img_path, img)
+cv2.imwrite(result_img_path, img)
