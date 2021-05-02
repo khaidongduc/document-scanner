4dad49dd9dee80fc7e24a69bcd1629857d34b634
khaidongduc <khaidongduc@gmail.com>
2021-05-02 12:54:42 +0700

    add sample, result: failed due to disortion from the the paper

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ document_scanner = DocumentScanner()
 sample_folder = "sample"
 result_folder = "result"
 
-file_name = "sample5.jpg"
+file_name = "sample6.jpg"
 sample_img_path = os.path.join(sample_folder, file_name)
 original_img = cv2.imread(sample_img_path)
 img = document_scanner.scan_document(original_img)
